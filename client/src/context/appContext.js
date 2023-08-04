82c78ac478368a06ccbaf084e3d9a654e2c2acfa
Aditya1711-tech <adityakaneriya1711@gmail.com>
2023-08-04 23:36:51 +0530

    Backend url updated

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -63,7 +63,7 @@ const AppProvider = ({ children }) => {
   };
 
   const authFetch = axios.create({
-    baseURL: "http://localhost:5001/",
+    baseURL: "https://wealth-wise.onrender.com/",
   });
 
   authFetch.interceptors.request.use(
